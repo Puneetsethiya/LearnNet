1a0ee984a8936d9bf997f97c82e3b79c9919e36c
puneet sethiya <puneetsethiya16@gmail.com>
2026-02-03 03:01:16 +0530

    LearnNet

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// serve frontend
-app.use(express.static(path.join(__dirname, "../public")));
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/auth.html"));
 });
+
+
+// serve frontend
+app.use(express.static(path.join(__dirname, "../public")));
+
 
 // routes
 const coursesRouter = require('./routes/courses');
